21e9a3796d8a76f54b8f62c3e6c3276340b1a304
tumi267 <itu.matlala4@gmail.com>
2025-08-21 22:26:20 +0200

    hard redirect

--- src/app/components/LoginUser/LoginUser.js
+++ src/app/components/LoginUser/LoginUser.js
@@ -32,7 +32,10 @@ function LoginUser() {
     </div>:
     <p className={styles.btn} onClick={()=>{setSignIn(true)}}>Log in/sign up</p>}
     {openSignIn&&<div className={styles.signInMod}>
-        <SignIn routing="hash"/>
+        <SignIn routing="hash"
+        signInForceRedirectUrl="https://ecom-template-kappa.vercel.app/"
+        signInFallbackRedirectUrl="https://ecom-template-kappa.vercel.app/"
+        />
         <button className={styles.Btn} onClick={()=>{setSignIn(false)}}>x</button>
     </div>}
     </>
