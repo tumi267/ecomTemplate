5ac41e5c0d7b98cd38e1e92da46e9d2767d80101
tumi267 <itu.matlala4@gmail.com>
2025-08-23 02:54:46 +0200

    clerk err

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,15 +1,11 @@
-import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+// middleware.ts
+import { clerkMiddleware } from '@clerk/nextjs/server'
 
 export default clerkMiddleware()
 
-  
 export const config = {
   matcher: [
-    // Apply middleware to everything except public routes
     '/((?!_next/static|_next/image|favicon.ico|sign-in|sign-up|api/createUser).*)',
-    // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
-    '/(api|trpc)(.*)',
+    '/api/:path*',
   ],
 }
