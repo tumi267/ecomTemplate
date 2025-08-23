7cb1de576c7474f15f007ed8a3a1ea5c3029cec1
tumi267 <itu.matlala4@gmail.com>
2025-08-23 03:18:32 +0200

    clerk err test

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,11 @@
 // middleware.ts
-// import { clerkMiddleware } from '@clerk/nextjs/server';
+import { clerkMiddleware } from '@clerk/nextjs/server';
 
-// export default clerkMiddleware();
+export default clerkMiddleware();
 
-// export const config = {
-//   matcher: [
-//     // Match everything except Next.js internals, API routes, auth pages, and static files
-//     '/((?!_next/static|_next/image|favicon.ico|api|sign-in|sign-up).*)',
-//   ],
-// };
+export const config = {
+  matcher: [
+    // Match everything except Next.js internals, API routes, auth pages, and static files
+    '/((?!_next/static|_next/image|favicon.ico|api|sign-in|sign-up).*)',
+  ],
+};
